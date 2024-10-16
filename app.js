d3a46ef16ea0184f22093b2cf8e1e40366f703d6
Vladimir612 <vladimir.12.lazarevic@gmail.com>
2024-10-16 20:27:44 +0200

    Izmene za lakija

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import apiRoutes from "./routes/index.js";
 import { dbConnectMiddleware } from "./middlewares/dbConnect.js";
-import connectToDatabase from "./config/DatabaseConfig.js";
+// import connectToDatabase from "./config/DatabaseConfig.js";
 
 dotenv.config();
 
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors());
 
 // Ensure database is connected before handling any request
-// app.use(dbConnectMiddleware);
+app.use(dbConnectMiddleware);
 
 app.get("/", (_, res) => {
   res.send("Hello, World!");
@@ -21,16 +21,16 @@ app.get("/", (_, res) => {
 
 app.use("/api", apiRoutes);
 
-const port = process.env.PORT || 5000;
-app.listen(port, async () => {
-  try {
-    await connectToDatabase();
-    console.log(`Server sluša na portu: ${port}`);
-  } catch (error) {
-    console.error("Neuspješno povezivanje s bazom podataka", error);
-    process.exit(1);
-  }
-});
+// const port = process.env.PORT || 5000;
+// app.listen(port, async () => {
+//   try {
+//     await connectToDatabase();
+//     console.log(`Server sluša na portu: ${port}`);
+//   } catch (error) {
+//     console.error("Neuspješno povezivanje s bazom podataka", error);
+//     process.exit(1);
+//   }
+// });
 
 // Export the app for Vercel to handle
 export default app;
