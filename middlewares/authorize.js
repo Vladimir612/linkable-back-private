a087fad209cfeb6b236ea3d2089dae5c904dc266
Lazar Cvetkovic <laki.cvetkovic@gmail.com>
2024-11-01 10:47:46 +0100

    Auth fixed

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -1,6 +1,10 @@
 import jwt from "jsonwebtoken";
 import User from "../schemas/UserSchema.js";
 
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const authorize = (allowedRoles) => {
   return async (req, res, next) => {
     const token = req.headers.authorization;
@@ -12,23 +16,33 @@ const authorize = (allowedRoles) => {
     }
 
     try {
+      console.log(process.env.JWT_SECRET);
+
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       const userIdFromToken = decoded.id;
-
+      console.log("1");
+      
       const user = await User.findById(userIdFromToken);
+      console.log("2");
 
       if (!user) {
         return res.status(401).json({ error: "Access denied. Invalid token." });
       }
+      console.log("3");
 
       if (!allowedRoles.includes(user.role)) {
         return res
           .status(403)
           .json({ error: "Access denied. Insufficient permissions." });
       }
+      console.log("4");
 
       req.user = user;
+      console.log("5");
+
       next();
+      console.log("6");
+
     } catch (error) {
       res.status(400).json({ error: "Invalid token." });
     }
